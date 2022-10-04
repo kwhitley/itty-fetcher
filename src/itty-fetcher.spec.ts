2548aa4e34967f8012127c1a6c56fe8e661ad912
Dana Woodman <dana@danawoodman.com>
2022-10-04 12:55:11 -0700

    Rewrite tests to be table-driven, add tests for passing in FormData

--- src/itty-fetcher.spec.ts
+++ src/itty-fetcher.spec.ts
@@ -1,30 +1,11 @@
 import fetchMock from 'fetch-mock'
 import 'isomorphic-fetch'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { fetcher, FetcherOptions } from './itty-fetcher'
-
-// DEFINE MOCKS
-const URL_BASE = 'https://foo.bar/'
-const URL_JSON = 'https://foo.bar/json'
-const URL_STRING = 'https://foo.bar/string'
-const URL_ERROR = 'https://foo.bar/error'
-
-const JSON_RESPONSE = ['apple', 'bat', 'cat']
-const STRING_RESPONSE = 'https://foo.bar/string'
-const ERROR_RESPONSE = 400
-
-const defaults = fetcher()
+import { beforeEach, describe, expect, it } from 'vitest'
+import { fetcher, FetcherOptions, RequestPayload } from './itty-fetcher'
 
 describe('fetcher', () => {
   beforeEach(() => {
     fetchMock.reset()
-    fetchMock
-      .get(URL_JSON, JSON_RESPONSE)
-      .get(URL_STRING, STRING_RESPONSE)
-      .get(URL_ERROR, ERROR_RESPONSE)
-      .patch(URL_JSON, JSON_RESPONSE, {
-        headers: { 'content-type': 'application/json' },
-      })
   })
 
   it('default import is a function', () => {
@@ -34,235 +15,315 @@ describe('fetcher', () => {
   describe('config options', () => {
     describe('base', () => {
       it("defaults to ''", () => {
-        expect(defaults.base).toBe('')
+        expect(fetcher().base).toBe('')
       })
 
       it('properly extends fetcher', () => {
-        const api = fetcher({ base: URL_BASE })
-
-        expect(api.base).toBe(URL_BASE)
-      })
-
-      it('properly prepends future fetch calls', async () => {
-        const api = fetcher({ base: URL_BASE })
-
-        const response = await api.get('json')
-
-        expect(response).toEqual(JSON_RESPONSE)
+        const base = 'https://google.com'
+        const api = fetcher({ base })
+        expect(api.base).toBe(base)
       })
     })
 
     describe('autoParse', () => {
       it('defaults to true', () => {
-        expect(defaults.autoParse).toBe(true)
+        expect(fetcher().autoParse).toBe(true)
       })
 
       it('properly extends fetcher', () => {
         const api = fetcher({ autoParse: false })
-
         expect(api.autoParse).toBe(false)
       })
 
       it('if set to false, leaves Response intact as Promise return', async () => {
-        const response: object = await fetcher({ autoParse: false }).get(URL_JSON)
-
+        fetchMock.get('/', {})
+        const response: object = await fetcher({ autoParse: false }).get('/')
         expect(response.constructor.name).toBe('Response')
       })
-    })
-
-    describe('transformRequest', () => {
-      const base = 'https://foo.com'
-      const tests: Record<
-        string,
-        {
-          payload?: any
-          init?: RequestInit
-          url?: string
-          transformRequest: FetcherOptions['transformRequest']
-          expected: any
-        }
-      > = {
-        'can set a header on request (no path)': {
-          transformRequest(req) {
-            req.headers['Foo'] = 'bar'
-            return req
-          },
-          expected: { url: base + '/', headers: { Foo: 'bar' } },
-        },
-        'can set a header on request (with path)': {
-          transformRequest(req) {
-            req.headers['Foo'] = 'bar'
-            return req
-          },
-          url: '/foo',
-          expected: { url: base + '/foo', headers: { Foo: 'bar' } },
-        },
-        'can add a query param to the URL': {
-          transformRequest(req) {
-            const url = new URL(req.url)
-            url.searchParams.set('message', 'hello world')
-            req.url = url.toString()
-            return req
-          },
-          expected: { url: base + '/?message=hello+world' },
-        },
-        'combines query params from the URL and the payload (object)': {
-          payload: { foo: 10 },
-          transformRequest(req) {
-            const url = new URL(req.url)
-            url.searchParams.set('message', 'hello world')
-            req.url = url.toString()
-            return req
-          },
-          expected: { url: base + '/?foo=10&message=hello+world' },
-        },
-        'combines query params from the URL and the payload (URLSearchParams)': {
-          payload: new URLSearchParams([
-            ['foo', '10'],
-            ['bar', '20'],
-          ]),
-          transformRequest(req) {
-            const url = new URL(req.url)
-            url.searchParams.set('message', 'hello world')
-            req.url = url.toString()
-            return req
-          },
-          expected: { url: base + '/?foo=10&bar=20&message=hello+world' },
-        },
-        'combines default headers with request headers': {
-          init: { headers: { B: 'b' } },
-          transformRequest(req) {
-            req.headers['A'] = 'a'
-            return req
-          },
-          expected: { url: base + '/', headers: { A: 'a', B: 'b' } },
-        },
-        'replace the origin of a request': {
-          transformRequest(req) {
-            req.url = req.url.replace('foo.com', 'bar.com')
-            return req
-          },
-          expected: { url: 'https://bar.com/' },
-        },
-      }
-
-      for (const [name, t] of Object.entries(tests)) {
-        it(name, async () => {
-          const mock = fetchMock.get(t.expected.url, {})
-
-          await fetcher({ base, transformRequest: t.transformRequest }).get(
-            t?.url ?? '',
-            t?.payload,
-            t?.init,
-          )
-
-          const [url, options] = mock.calls()[0]
-
-          expect(url).toEqual(t.expected.url)
-          expect(options?.headers).toHaveProperty('Content-Type', 'application/json')
-
-          if (t.expected?.headers) {
-            for (const [key, val] of Object.entries(t.expected.headers)) {
-              expect(options?.headers).toHaveProperty(key, val)
-            }
-          }
-        })
-      }
     })
   })
 
   describe('HTTP method calls - fetcher().get(url, payload?, options?)', () => {
     it('any other property returns a function', () => {
-      expect(typeof defaults.foo).toBe('function')
-    })
-
-    it('returns object data directly from requests', async () => {
-      const response = await fetcher().get(URL_JSON)
-
-      expect(response).toEqual(JSON_RESPONSE)
-    })
-
-    it('can access a property of the response data', async () => {
-      const response: string[] = await fetcher().get(URL_JSON)
-
-      expect(response[0]).toBe(JSON_RESPONSE[0])
-    })
-
-    it('will safely catch non-OK Responses', async () => {
-      const errorHandler = vi.fn()
-
-      await fetcher().get(URL_ERROR).catch(errorHandler)
-
-      expect(errorHandler).toHaveBeenCalled()
-    })
-
-    it('will autoparse to text if no json headers found in response', async () => {
-      const response = await fetcher().get(URL_STRING)
-
-      expect(response).toBe(STRING_RESPONSE)
+      expect(typeof fetcher().foo).toBe('function')
     })
 
     it('will honor TS Type definitions for response payloads', async () => {
       type ArrayOfNumbers = number[]
-      const response = await fetcher().get<ArrayOfNumbers>(URL_JSON)
-
-      expect(response).toEqual(JSON_RESPONSE)
+      const url = '/foo.json'
+      const data: ArrayOfNumbers = [1, 2, 3, 4, 5]
+      fetchMock.get(url, data)
+      await expect(fetcher().get<ArrayOfNumbers>(url)).resolves.toEqual(data)
     })
 
-    describe('GET', () => {
-      it('passes data into query params', async () => {
-        const url = 'https://google.com'
-        const data = { foo: 'hello world!', baz: 10, biz: true, bop: ['a', 'b'] }
+    const base = 'https://foo.com'
 
-        const expected = new URL(url)
-        for (const [key, val] of Object.entries(data)) {
-          expected.searchParams.set(key, String(val))
+    const formdata = new FormData()
+    formdata.append('foo', 'bar')
+
+    const tests: {
+      [key: string]: {
+        method?: string
+        url?: string
+        payload?: RequestPayload
+        status?: number
+        init?: RequestInit
+        response?: any
+        options?: FetcherOptions
+        expected: {
+          url?: string
+          response?: any
+          body?: any
+          headers?: Record<string, string>
+          error?: string
+        }
+      }
+    } = {
+      // Basic response types
+      'will auto-parse JSON (array)': {
+        response: JSON.stringify(['apple', 'bat', 'cat']),
+        expected: { response: ['apple', 'bat', 'cat'] },
+      },
+      'will auto-parse JSON (object)': {
+        response: JSON.stringify({ a: 'A', b: 10, c: true }),
+        expected: { response: { a: 'A', b: 10, c: true } },
+      },
+      'will auto-parse to text if no json headers found in response': {
+        init: { headers: { 'content-type': 'text/plain' } },
+        response: JSON.stringify({ foo: 'bar' }),
+        expected: { response: '{"foo":"bar"}' },
+      },
+
+      // Base
+      'properly prepends future fetch calls': {
+        url: '/foo.json',
+        options: { base: 'https://foo.com' },
+        expected: { url: 'https://foo.com/foo.json' },
+      },
+
+      // Passing headers in init
+      'will still embed content-type header if headers are included in fetch options': {
+        method: 'patch',
+        init: { headers: { A: 'a' } },
+        expected: { headers: { A: 'a', 'Content-Type': 'application/json' } },
+      },
+
+      // FormData
+      'will pass FormData as-is (will not JSON.stringify)': {
+        method: 'post',
+        payload: formdata,
+        expected: { body: formdata },
+      },
+
+      // GET query param handling
+      'will pass data into query params if a GET request': {
+        payload: { foo: 'hello world' },
+        expected: { body: undefined, url: '/?foo=hello+world' },
+      },
+      'will pass in URLSearchParams payload if a GET request': {
+        payload: params_from_object({ a: 'A', b: 10, c: true, d: [1, 2] }),
+        expected: {
+          body: undefined,
+          url: '/?' + params_from_object({ a: 'A', b: 10, c: true, d: [1, 2] }).toString(),
+        },
+      },
+
+      // Error handling
+      'will throw on non-OK Responses (400)': {
+        status: 400,
+        expected: { error: 'Bad Request' },
+      },
+      'will throw on non-OK Responses (500)': {
+        status: 500,
+        expected: { error: 'Internal Server Error' },
+      },
+
+      // transformRequest
+      'can set a header on request (no path)': {
+        options: {
+          transformRequest(req) {
+            req.headers['Foo'] = 'bar'
+            return req
+          },
+        },
+        expected: { headers: { Foo: 'bar' } },
+      },
+      'can set a header on request (with path)': {
+        url: '/foo',
+        options: {
+          transformRequest(req) {
+            req.headers['Foo'] = 'bar'
+            return req
+          },
+        },
+        expected: { url: '/foo', headers: { Foo: 'bar' } },
+      },
+      'can add a query param to the URL': {
+        options: {
+          base,
+          transformRequest(req) {
+            const url = new URL(req.url)
+            url.searchParams.set('message', 'hello world')
+            req.url = url.toString()
+            return req
+          },
+        },
+        expected: { url: base + '/?message=hello+world' },
+      },
+      'combines query params from the URL and the payload (object)': {
+        payload: { foo: 10 },
+        options: {
+          base,
+          transformRequest(req) {
+            const url = new URL(req.url)
+            url.searchParams.set('message', 'hello world')
+            req.url = url.toString()
+            return req
+          },
+        },
+        expected: { url: base + '/?foo=10&message=hello+world' },
+      },
+      'combines query params from the URL and the payload (URLSearchParams)': {
+        payload: new URLSearchParams([
+          ['foo', '10'],
+          ['bar', '20'],
+        ]),
+        options: {
+          base,
+          transformRequest(req) {
+            const url = new URL(req.url)
+            url.searchParams.set('message', 'hello world')
+            req.url = url.toString()
+            return req
+          },
+        },
+        expected: { url: base + '/?foo=10&bar=20&message=hello+world' },
+      },
+      'combines default headers with request headers': {
+        init: { headers: { B: 'b' } },
+        options: {
+          transformRequest(req) {
+            req.headers['A'] = 'a'
+            return req
+          },
+        },
+        expected: { headers: { A: 'a', B: 'b' } },
+      },
+      'replace the origin of a request': {
+        options: {
+          base,
+          transformRequest(req) {
+            req.url = req.url.replace('foo.com', 'bar.com')
+            return req
+          },
+        },
+        expected: { url: 'https://bar.com/' },
+      },
+    }
+
+    for (const [name, t] of Object.entries(tests)) {
+      it(name, async () => {
+        const {
+          method = 'get',
+          options,
+          init = {},
+          response = {},
+          payload,
+          status,
+          expected,
+          url = '',
+        } = t
+
+        // Construct a mock URL that takes into account all the properties that
+        // will get combined into the final URL.
+        const full_url = create_mock_url({
+          expected_url: expected?.url,
+          url,
+          options,
+          method,
+          payload,
+        })
+
+        // If the test is expecting an error, then we'll mock the fetch to
+        // throw an error.
+        if (expected.error && status) {
+          fetchMock.mock(full_url, status)
+          await expect(fetcher(options)[method](url, payload, init)).rejects.toThrow(expected.error)
+          return
         }
 
-        const mock = fetchMock.get(expected.toString(), data)
-
-        await fetcher().get(url, data)
-
-        const [uri, init] = mock.calls()[0]
-        expect(uri).toEqual(expected.toString())
-        expect(init?.body).toBeUndefined()
-      })
-
-      it('can pass in custom URLSearchParams', async () => {
-        const url = 'https://google.com'
-        const data = { foo: 'hello world!', baz: 10, biz: true, bop: ['a', 'b'] }
-        const params = new URLSearchParams()
-        params.set('foo', data.foo)
-        params.set('baz', String(data.baz))
-        params.set('biz', String(data.biz))
-        params.append('bop', data.bop[0])
-        params.append('bop', data.bop[1])
-
-        const expected = new URL(url)
-        expected.search = params.toString()
-
-        const mock = fetchMock.get(expected.toString(), [])
-
-        await fetcher().get(url, params)
-
-        const [uri, init] = mock.calls()[0]
-        expect(uri).toEqual(expected.toString())
-        expect(init?.body).toBeUndefined()
-      })
-    })
-
-    describe('options (use native fetch options)', () => {
-      it('will still embed content-type header if headers are included in fetch options', async () => {
-        const response = await fetcher().patch(
-          URL_JSON,
-          {},
-          {
-            headers: { Authorization: 'Bearer of.good.news' },
+        const mock = fetchMock[method](full_url, {
+          body: response,
+          ...init,
+          headers: {
+            'content-type': 'application/json',
+            ...init.headers,
           },
-        )
+        })
 
-        expect(response).toEqual(JSON_RESPONSE)
+        const resp = await fetcher(options)[method](url, payload, init)
+
+        const [actual_url, actual_init] = mock.calls()[0]
+
+        if (expected?.response) expect(resp).toEqual(expected.response)
+        expect(actual_init?.method).toEqual(method.toUpperCase())
+        if (expected?.url) expect(actual_url).toEqual(expected.url)
+        if (expected?.body) expect(actual_init?.body).toEqual(expected.body)
+
+        // Assert that the proper headers are set.
+        if (expected?.headers) {
+          for (const [key, val] of Object.entries(expected.headers)) {
+            expect(actual_init?.headers).toHaveProperty(key, val)
+          }
+        }
       })
-    })
+    }
   })
 })
+
+function params_from_object(obj: object) {
+  const params = new URLSearchParams()
+  for (const [k, v] of Object.entries(obj)) {
+    if (Array.isArray(v)) {
+      for (const item of v) {
+        params.append(k, item)
+      }
+    } else {
+      params.set(k, v)
+    }
+  }
+  return params
+}
+
+function create_mock_url({
+  expected_url,
+  url,
+  method,
+  payload,
+  options,
+}: {
+  expected_url?: string
+  url?: string
+  method?: string
+  payload?: RequestPayload
+  options?: FetcherOptions
+} = {}) {
+  if (expected_url) return expected_url
+
+  let mock_url = url ?? ''
+
+  // Append the base URL if it is provided.
+  if (options?.base) mock_url = options.base + mock_url
+
+  // For GET requests which have a payload, we need to assert against the
+  // query params in the URL. To do this, we need to evaluate the payload
+  // and add it to the URL.
+  if (method === 'get' && payload && typeof payload === 'object' && !options?.transformRequest) {
+    const url = new URL(mock_url)
+    url.search = (
+      payload instanceof URLSearchParams ? payload : params_from_object(payload)
+    ).toString()
+    mock_url = url.toString()
+  }
+  return mock_url
+}
