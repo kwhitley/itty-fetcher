638e8faaab879ad6d0eab45cd65c7973c22999d6
Kevin Whitley <krwhitley@gmail.com>
2025-07-06 15:34:56 -0500

    stripped intermediate

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -70,22 +70,44 @@ const handleRequest = async (
   return response
 }
 
+// export const fetcher = (
+//   optionsOrBase?: FetcherOptions,
+//   additionalOptions?: FetcherOptionsObject
+// ): Fetcher => {
+//   let {
+//     base = globalThis.location?.origin || '',
+//     headers = {},
+//     ...restOptions
+//   } = typeof optionsOrBase == 'string'
+//     ? { base: optionsOrBase, ...additionalOptions }
+//     : optionsOrBase || {}
+
+//   // @ts-ignore
+//   return new Proxy(() => {}, {
+//     get: (target, prop: 'get' | 'post' | 'put' | 'patch' | 'delete') => (...args: any[]) =>
+//       // @ts-ignore
+//       handleRequest(prop.toUpperCase(), args, restOptions, base, headers)
+//   })
+// }
+
 export const fetcher = (
   optionsOrBase?: FetcherOptions,
   additionalOptions?: FetcherOptionsObject
 ): Fetcher => {
-  let {
-    base = globalThis.location?.origin || '',
-    headers = {},
-    ...restOptions
-  } = typeof optionsOrBase == 'string'
+  let opts = typeof optionsOrBase == 'string'
     ? { base: optionsOrBase, ...additionalOptions }
     : optionsOrBase || {}
 
   // @ts-ignore
   return new Proxy(() => {}, {
-    get: (target, prop: 'get' | 'post' | 'put' | 'patch' | 'delete') => (...args: any[]) =>
-      // @ts-ignore
-      handleRequest(prop.toUpperCase(), args, restOptions, base, headers)
+    get: (target, prop: 'get' | 'post' | 'put' | 'patch' | 'delete') => (...args: any) =>
+      handleRequest(
+        prop.toUpperCase(),
+        args,
+        opts,
+        // @ts-ignore
+        opts.base || globalThis.location?.origin || '',
+        opts.headers || {}
+      )
   })
 }
